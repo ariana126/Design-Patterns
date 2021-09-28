e1801ba615e62d852859d4632f0be30931563fa0
Ariana <ariana.maghsoudi82@gmail.com>
2021-09-28 19:17:15 +0330

    Facade - PHP - Order Registration

---
+++ Facade/PHP/Order Registration/AmazonPackage.php
@@ -0,0 +1,55 @@
+<?php
+
+class Order 
+{
+    private string $id;
+
+    private int $price;
+
+    public function __construct(string $id)
+    {
+        $this->id   = $id;
+        $this->price = 0;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setPrice(int $price): void
+    {
+        $this->price = $price;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+}
+
+class PriceCalculator
+{
+
+    public function calculatePriceFor(Order $order): int
+    {
+        return rand(3, 10);
+    }
+
+}
+
+class AmazonCommunicationService
+{
+
+    public function registerOrder(Order $order): void
+    {
+        echo 'Order with Id: ' . $order->getId() . ' and price: ' . $order->getPrice() . ' registered.' . PHP_EOL;
+    }
+
+    public function registerUser(): void
+    {
+        echo PHP_EOL;
+    }
+
+}
---
+++ Facade/PHP/Order Registration/App.php
@@ -0,0 +1,36 @@
+<?php
+
+require './AmazonPackage.php';
+
+class AmazonOrderRegistrationService
+{
+    
+    private PriceCalculator $priceCalculator;
+
+    private AmazonCommunicationService $amazonCommunicationService;
+
+    public function __construct()
+    {
+        $this->priceCalculator   = new PriceCalculator;
+        $this->amazonCommunicationService   = new AmazonCommunicationService;
+    }
+
+    public function register(string $id): void
+    {
+        $order = new Order($id);
+
+        $order->setPrice(
+            $this->priceCalculator->calculatePriceFor($order)
+        );
+
+        $this->amazonCommunicationService->registerOrder($order);
+    }
+
+}
+
+
+$orderRegistrationService = new AmazonOrderRegistrationService();
+
+$orderRegistrationService->register('Order-1');
+$orderRegistrationService->register('Order-2');
+$orderRegistrationService->register('Order-3');
